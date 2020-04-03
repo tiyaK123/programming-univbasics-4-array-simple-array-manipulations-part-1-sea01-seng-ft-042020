b62e5de26f871c4a30ad4afda2cc418a924e12e5
tiya adugna <tiyakefalee@gmail.com>
2020-04-03 08:21:04 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_push(arr,str)
+  arr.push(str)
+end
+def using_unshift(arr,str)
+  arr.unshift(str)
+end 
+def using_pop(arr)
+  arr.pop 
+end
+def using_shift(arr)
+  arr.shift  
+end
+def pop_with_args(arr)
+  arr.pop(2)
+end
+def shift_with_args(arr)
+  arr.shift(2)
+end
